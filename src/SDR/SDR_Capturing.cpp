fd9b16aa5cd4aba9b26f230ce17964bb79a9b2eb
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-07-04 23:05:49 -0400

    Create SDR_Capturing.cpp

---
+++ src/SDR/SDR_Capturing.cpp
@@ -0,0 +1,15 @@
+#include <SoapySDR/Version.hpp>
+#include <SoapySDR/Modules.hpp>
+#include <SoapySDR/Registry.hpp>
+#include <SoapySDR/Device.hpp>
+#include <SoapySDR/ConverterRegistry.hpp>
+#include <iostream>
+using namespace std;
+
+#define okay(msg, ...) printf("[+] " msg "\n", ##__VA_ARGS__)
+#define warn(msg, ...) printf("[-] " msg "\n", ##__VA_ARGS__)
+#define info(msg, ...) printf("[i] " msg "\n", ##__VA_ARGS__)
+
+int capturing() {
+
+}
