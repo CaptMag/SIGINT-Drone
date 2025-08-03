d38a9c0364e5de497bc75eb092e5ecc3888fb513
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-08-02 20:17:56 -0400

    V0.1.2 Alpha
    Slight update in print statements and organization

--- src/SDR/UTILS.cpp
+++ src/SDR/UTILS.cpp
@@ -1,38 +1,45 @@
-#include "UTILS.h"
+#include "enumeration.h"
 
 std::vector<std::string> Formatting()
 {
 	/*---[Grab Metadata from available SDRs]---*/
 
 		SoapySDR::KwargsList enumerate = SoapySDR::Device::enumerate();
-		okay("Devices found! Creating formatted drivers....");
-		vector<string> SDR_MakeDevices;
-		for (auto SDR = enumerate.begin(); SDR != enumerate.end(); ++SDR) {
-			info("Device Information: %s", SDR->at("driver"));
-			printf("---/*Checking Device Compatability...*/---\n");
-			string SDR_driver;
-			string SDR_serial;
-			string SDR_MakeDevice;
-			try {
-				SDR_serial = SDR->at("serial");
-				SDR_driver = SDR->at("driver");
-				SDR_MakeDevice = "driver=" + SDR_driver + ",serial=" + SDR_serial;
-				SDR_MakeDevices.push_back(SDR_MakeDevice);
-				printf("[*] Total formatted device strings: %zu\n", SDR_MakeDevices.size());
-				string SDR_label = SDR->at("label");
-				string SDR_part_id = SDR->at("part_id");
-				string SDR_version = SDR->at("version");
-
-				string SDR_complete = SDR_driver + " " + SDR_serial + " " + SDR_label + " " + SDR_part_id + " " + SDR_version;
-				okay("Complete information for all devices: %s", SDR_complete.c_str());
-
-			}
-			catch (const std::out_of_range& e) {
-				warn("Exception caught: %s", e.what());
-				continue;
-			}
+		if (enumerate.empty()) {
+			warn("No devices found! Please check connections...");
 		}
-		return SDR_MakeDevices;
+		else {
+
+			okay("Devices found! Creating formatted drivers....");
+			vector<string> SDR_MakeDevices;
+			printf("---/*Checking Device Compatability...*/---\n");
+			for (auto SDR = enumerate.begin(); SDR != enumerate.end(); ++SDR) {
+
+				string SDR_driver;
+				string SDR_serial;
+				string SDR_MakeDevice;
+				try {
+
+					SDR_serial = SDR->at("serial");
+					SDR_driver = SDR->at("driver");
+					SDR_MakeDevice = "driver=" + SDR_driver + ",serial=" + SDR_serial;
+					SDR_MakeDevices.push_back(SDR_MakeDevice);
+					printf("[*] Total formatted device strings: %zu\n", SDR_MakeDevices.size());
+					string SDR_label = SDR->at("label");
+					string SDR_part_id = SDR->at("part_id");
+					string SDR_version = SDR->at("version");
+
+					string SDR_complete = SDR_driver + " " + SDR_serial + " " + SDR_label + " " + SDR_part_id + " " + SDR_version;
+					okay("Complete information for all devices: %s", SDR_complete.c_str());
+
+				}
+				catch (const std::out_of_range& e) {
+					warn("Exception caught: %s", e.what());
+					continue;
+				}
+			}
+			return SDR_MakeDevices;
+		}
 }
 
 
