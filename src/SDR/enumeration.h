7b61068c74c8d4a33661a8f657998142f5d89384
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-08-02 20:17:09 -0400

    V0.1.2 Alpha
    Added more function calling

--- src/SDR/enumeration.h
+++ src/SDR/enumeration.h
@@ -2,6 +2,12 @@
 #ifndef ENUMERATION_H
 #define ENUMERATION_H
 
+#include "UTILS.h"
+
+std::vector<std::string> Formatting();
+SoapySDR::Device* MakeDev();
 int enumeration();
+std::vector<std::string> ListSensors(SoapySDR::Device* device, const int dir, const size_t chan);
+std::vector<std::string> Range_and_Tuning(SoapySDR::Device* device, const int dir, const size_t chan);
 
 #endif#pragma once
