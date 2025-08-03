f7710b359546e120b934516606007d3b52226a94
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-08-02 20:16:40 -0400

    V0.1.2 Alpha
    Added function calling as well as debugging code in case of troubleshooting

--- src/SDR/main.cpp
+++ src/SDR/main.cpp
@@ -1,9 +1,24 @@
-#include "UTILS.h"
 #include "enumeration.h"
+#include "UTILS.h"
+
+/*If needed, uncomment these lines for troubleshooting*/
+
+	//info("Default module path: %s", SoapySDR::getRootPath().c_str());
+
+
+	//auto mods = SoapySDR::listModules();
+	//for (auto mod : mods) {
+	//	info("Loaded Module: %s", mod.c_str());
+	//}
 
 int main() {
 
-	enumeration();
+	_putenv("SOAPY_SDR_PLUGIN_PATH=C:\\Program Files\\PothosSDR\\lib\\SoapySDR\\modules0.8");
 
+	std::vector<std::string> Formatting();
+	SoapySDR::Device* MakeDev();
+	int enumeration();
+	std::vector<std::string> sensors = ListSensors(MakeDev(), SOAPY_SDR_TX, 1);
+	std::vector<std::string> Compledted_Information = Range_and_Tuning(MakeDev(), SOAPY_SDR_TX, 0);
 	return EXIT_SUCCESS;
 }
