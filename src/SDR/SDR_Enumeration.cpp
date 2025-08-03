5acf0fbfceee7b682365a64ed0ee87c835039d23
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-08-02 20:18:51 -0400

    V0.1.2 Alpha
    Changed Device make into its own function to ensure it can be ready for use, and not unmade prematurely

--- src/SDR/SDR_Enumeration.cpp
+++ src/SDR/SDR_Enumeration.cpp
@@ -1,4 +1,3 @@
-#include "UTILS.h"
 #include "enumeration.h"
 
 int enumeration() {
@@ -6,29 +5,15 @@ int enumeration() {
 
 		/*---[Create device for SDR probbing]---*/
 
-		std::vector<std::string> SDR_MakeDevices = Formatting();
+		SoapySDR::Device* device = MakeDev();
 
-		for (const auto& SDR_MakeDevice : SDR_MakeDevices) {
-			try {
-				info("Make Device: %s", SDR_MakeDevice.c_str());
-				auto device = SoapySDR::Device::make(SDR_MakeDevice);
-				if (!device) {
-					warn("Could not create device for %s", SDR_MakeDevice.c_str());
-					continue;
-				}
-				info("Driver= %s", device->getDriverKey().c_str());
-				info("Hardware= %s", device->getHardwareKey().c_str());
-				for (const auto& it : device->getHardwareInfo()) {
-					std::cout << " " << it.first << "=" << it.second << std::endl;
-				}
-				SoapySDR::Device::unmake(device);
-			}
-			catch (const std::exception& ex) {
-				warn("Error making device: %s", ex.what());
-				continue;
-			}
-
+		info("Driver= %s", device->getDriverKey().c_str());
+		info("Hardware= %s", device->getHardwareKey().c_str());
+		for (const auto& it : device->getHardwareInfo()) {
+			std::cout << " " << it.first << "=" << it.second << std::endl;
 		}
+		SoapySDR::Device::unmake(device);
+
 	}
 	catch (std::exception& e) {
 		warn("Exception caught: %s", e.what());
@@ -37,3 +22,4 @@ int enumeration() {
 
 	return EXIT_SUCCESS;
 }
+
