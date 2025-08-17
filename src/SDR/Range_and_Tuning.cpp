1ef2c5752fcc1b356ab4579f76ecb33ea317361a
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-08-17 15:57:26 -0400

    V2.1.0 Alpha
    Added StreamFormats, GetClockSource, GetBandwidths

--- src/SDR/Range_and_Tuning.cpp
+++ src/SDR/Range_and_Tuning.cpp
@@ -4,11 +4,17 @@
 
 	/*Function designed to check for all 'fine-tunning' and more information about an SDR before data capturing*/
 
+
+/*Amount of Available Channels*/
+
 std::string GetChannelNum(SoapySDR::Device* device, int dir)
 {
 	size_t chan_amt = device->getNumChannels(dir);
 	return "[i] Total Number of Channels: " + std::to_string(chan_amt);
 }
+
+
+/*Duplexity Checker (either full or half)*/
 
 std::string chk_duplex(SoapySDR::Device* device, int dir, size_t chan)
 {
@@ -34,13 +40,15 @@ std::string chk_duplex(SoapySDR::Device* device, int dir, size_t chan)
 	}
 }
 
+
+/*List Sample Rate Ranges*/
+
 std::string Sample_Rates(SoapySDR::Device* device, int dir, size_t chan)
 {
-
 	try {
 
 		std::vector<double> samp_rate = device->listSampleRates(dir, chan);
-		std::string result = "[+] Supported Sample Rates:\n";
+		std::string result = "===============[Supported Sample Rates]===============\n";
 
 		for (const auto& rates : samp_rate)
 		{
@@ -56,13 +64,15 @@ std::string Sample_Rates(SoapySDR::Device* device, int dir, size_t chan)
 	}
 }
 
+
+/*Antenna Name and Information*/
 std::string ListAntennas(SoapySDR::Device* device, int dir, size_t chan)
 {
 
 	try {
 
 		std::vector<std::string> Antenna_Info = device->listAntennas(dir, chan);
-		std::string result = "[+] Antenna Info:\n";
+		std::string result = "================[Antenna Information]================\n";
 
 		for (const auto& names : Antenna_Info)
 		{
@@ -77,13 +87,40 @@ std::string ListAntennas(SoapySDR::Device* device, int dir, size_t chan)
 	}
 }
 
+
+/*Displays clock names*/
+
+std::string ClockSource(SoapySDR::Device* device)
+{
+	try {
+
+		std::vector<std::string> Available_Clocks = device->listClockSources();
+		std::string Get_Source = device->getClockSource();
+		std::string result = "===============[Supported Clock Sources]================ \n";
+
+		for (const auto& clock : Available_Clocks)
+		{
+			result += " -" + clock + "\n";
+		}
+		return result;
+	}
+	catch (std::exception& e)
+	{
+		warn("Exception Caught! %s", e.what());
+		return "[!] Error retrieving Clock Sourcing Info";
+	}
+}
+
+
+/*List Bandwidth Ranges*/
+
 std::string GetBandwidths(SoapySDR::Device* device, int dir, size_t chan)
 {
 
 	try {
 
 		std::vector<double> BandWidthRanges = device->listBandwidths(dir, chan);
-		std::string result = "[+] BandWidth Ranges:\n";
+		std::string result = "===============[List Bandwidth Ranges]===============\n";
 
 		for (const auto& ranges : BandWidthRanges)
 		{
@@ -98,9 +135,29 @@ std::string GetBandwidths(SoapySDR::Device* device, int dir, size_t chan)
 	}
 }
 
+std::string StreamFormats(SoapySDR::Device* device, const int dir, const size_t chan)
+{
+	try {
+
+		std::vector<std::string> Formats = device->getStreamFormats(dir, chan);
+		std::string result = "===============[Available Stream Formats]===============\n";
+
+		for (const auto& list : Formats)
+		{
+			result += " -" + list + "\n";
+		}
+		return result;
+	}
+	catch (std::exception& e)
+	{
+		warn("Exception Caught! %s", e.what());
+		return "[!] Error Retrieving Streaming Formats";
+	}
+}
+
 std::vector<std::string> Range_and_Tuning(SoapySDR::Device* device, const int dir, const size_t chan)
 {
-
+	
 	std::vector<std::string> Completed_Information;
 	try {
 
@@ -121,6 +178,10 @@ std::vector<std::string> Range_and_Tuning(SoapySDR::Device* device, const int di
 		Completed_Information.push_back(ListAntennas(device, dir, chan));
 
 		Completed_Information.push_back(GetBandwidths(device, dir, chan));
+
+		Completed_Information.push_back(ClockSource(device));
+
+		Completed_Information.push_back(StreamFormats(device, dir, chan));
 
 		for (const auto& item : Completed_Information)
 		{
