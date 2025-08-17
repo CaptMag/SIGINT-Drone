5586e0251473524dfb336b1cb4209da344657266
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-08-17 15:58:46 -0400

    V2.1.0 Alpha
    Support for StreamSetup and GUI

--- src/SDR/enumeration.h
+++ src/SDR/enumeration.h
@@ -9,5 +9,14 @@ SoapySDR::Device* MakeDev();
 int enumeration();
 std::vector<std::string> ListSensors(SoapySDR::Device* device, const int dir, const size_t chan);
 std::vector<std::string> Range_and_Tuning(SoapySDR::Device* device, const int dir, const size_t chan);
+SoapySDR::Stream* StreamSetup(SoapySDR::Device* device,
+	int direction,
+	const std::string& format,
+	const std::vector<size_t>& channels,
+	const SoapySDR::Kwargs& args,
+	const int flags = 0,
+	const long long timeNs = 0,
+	const size_t numElems = 0);
+int GUI(SoapySDR::Device* device, SoapySDR::Stream* stream);
 
 #endif#pragma once
