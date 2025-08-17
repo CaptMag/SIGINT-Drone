dc25dd985b763f93ec9ddff58aa8cd3d1b4a31ff
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-08-17 15:58:16 -0400

    V2.1.0 Alpha
    Added in StreamSetup and GUI to Main.cpp, organized it a bit more for better clarity

--- src/SDR/main.cpp
+++ src/SDR/main.cpp
@@ -13,12 +13,16 @@
 
 int main() {
 
+
 	_putenv("SOAPY_SDR_PLUGIN_PATH=C:\\Program Files\\PothosSDR\\lib\\SoapySDR\\modules0.8");
 
-	std::vector<std::string> Formatting();
-	SoapySDR::Device* MakeDev();
-	int enumeration();
-	std::vector<std::string> sensors = ListSensors(MakeDev(), SOAPY_SDR_TX, 1);
-	std::vector<std::string> Compledted_Information = Range_and_Tuning(MakeDev(), SOAPY_SDR_TX, 0);
+	std::vector<std::string> fmt = Formatting();
+	SoapySDR::Device* dev = MakeDev();
+	int enum_result = enumeration();
+	std::vector<std::string> sensors = ListSensors(dev, SOAPY_SDR_RX, 1);
+	std::vector<std::string> Completed_Information = Range_and_Tuning(dev, SOAPY_SDR_RX, 0);
+	SoapySDR::Stream* stream = StreamSetup(dev, SOAPY_SDR_RX, "CF32", { 0 }, {}, 0, 0, 0);
+	auto gui_result = GUI(dev, stream);
+
 	return EXIT_SUCCESS;
 }
